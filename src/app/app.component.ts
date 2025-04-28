1ae565ea52fedf2cadd5247f8b539eb8139fc92d
Luseramia <108973001+Luseramia@users.noreply.github.com>
2025-04-28 16:14:00 +0700

    first

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,76 @@
-import { Component } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { HttpService } from '../services/http-service';
+import {
+  FormControl,
+  FormsModule,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
+import { MatInputModule } from '@angular/material/input';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import {MatCheckboxModule} from '@angular/material/checkbox';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports:[
+    CommonModule,
+    MatInputModule,
+    MatFormFieldModule,
+    ReactiveFormsModule,
+    FormsModule,
+    MatCheckboxModule
+    ],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
-export class AppComponent {
-  title = 'todolist';
+export class AppComponent implements OnInit {
+  private httpService = inject(HttpService);
+  todolist = new FormControl('', [Validators.required]);
+  
+  async ngOnInit(): Promise<void> {
+    this.getTodoList();
+  }
+
+  async getTodoList(){
+    const result = await this.httpService.PostData<Todolist>('/getTodoList',{})
+    if("body" in result){
+      if (Array.isArray(result.body)) {
+        this.todoLists = result.body;
+      } 
+    }
+  }
+
+  async addTodoList() {
+    this.httpService.PostData('/insert-todolist', {
+      todolist: this.todolist.value,
+      isCheck: 0,
+    });
+    this.todolist.setValue("");
+    this.getTodoList();
+
+  }
+
+  async updateCheckTodo($completed: boolean, index: number,todolistId:string){
+    if($completed){
+    await this.httpService.PostData('/update-todolist', {
+      todolistId:todolistId,
+      isCheck: 1,
+    });
+    }
+    else{
+      await this.httpService.PostData('/update-todolist', {
+        todolistId:todolistId,
+      isCheck: 0,
+    });
+    }
+  }
+  public todoLists:Array<Todolist>  = [];
+}
+
+interface Todolist{
+  todolistId:string;
+  todolist:string | null;
+  isCheck:number;
 }
--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,9 +1,23 @@
 import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
-
+import { provideAnimations } from '@angular/platform-browser/animations';
 import { routes } from './app.routes';
-import { provideClientHydration, withEventReplay } from '@angular/platform-browser';
+import { provideClientHydration } from '@angular/platform-browser';
+import { provideHttpClient, withFetch, withInterceptors, HttpHandlerFn, HttpRequest } from '@angular/common/http';
+import { catchError, throwError } from 'rxjs';
+import { inject } from '@angular/core';
+import { Router } from '@angular/router';
+
+
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes), provideClientHydration(withEventReplay())]
+  providers: [
+    provideZoneChangeDetection({ eventCoalescing: true }),
+    provideRouter(routes),
+    provideClientHydration(),
+    provideAnimations(),
+    provideHttpClient(
+      withFetch(),
+    ),
+  ],
 };
